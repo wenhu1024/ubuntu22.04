5e03223fe8130d423d2bf3b2b61cf08a00098b2e
wenhu1024 <1340773297@qq.com>
2022-07-11 22:25:48 +0800

    modify

--- c/ch03/3.13.1.c
+++ c/ch03/3.13.1.c
@@ -4,16 +4,16 @@
 
 int main()
 {
-	printf("singed_char : MAX:%d\t MIN:%d\t \n",SCHAR_MAX,SCHAR_MIN);
-	printf("unsigned_char : MAX:%d\t MIN:%d\t \n",UCHAR_MAX,0);
+	printf("singed_char : MAX:%d\t MIN:%d\n",SCHAR_MAX,SCHAR_MIN);
+	printf("unsigned_char : MAX:%d\t MIN:%d\n",UCHAR_MAX,0);
 	putchar('\n');
-	printf("signed_short_int : MAX:%d\t MIN:%d\t \n",SHRT_MAX,SHRT_MIN);
-	printf("unsigned_short_int : MAX:%d\t MIN:%d\t \n",USHRT_MAX,0);
+	printf("signed_short_int : MAX:%d\t MIN:%d\n",SHRT_MAX,SHRT_MIN);
+	printf("unsigned_short_int : MAX:%d\t MIN:%d\n",USHRT_MAX,0);
 	putchar('\n');
-	printf("signed_int : MAX:%d\t MIN:%d\t \n",INT_MAX,INT_MIN);
-	printf("unsigned_int : MAX:%d\t MIN:%d\t \n",UINT_MAX,0);
+	printf("signed_int : MAX:%d\t MIN:%d\n",INT_MAX,INT_MIN);
+	printf("unsigned_int : MAX:%d\t MIN:%d\n",UINT_MAX,0);
 	putchar('\n');
-	printf("signed_long_int : MAX:%ld\t MIN:%ld\t \n",LONG_MAX,LONG_MIN);
-	printf("unsigned_long_int : MAX:%ld\t MIN:%ld\t \n",ULONG_MAX,0);
+	printf("signed_long_int : MAX:%ld\t MIN:%ld\n",LONG_MAX,LONG_MIN);
+	printf("unsigned_long_int : MAX:%llu\t MIN:%llu\n",ULONG_MAX,0);
 	return EXIT_SUCCESS;
 }
